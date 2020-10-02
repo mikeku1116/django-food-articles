c6c32aeab87af50ca134f635de31b8263ffa7895
mikeku1116 <mikekuforgit@gmail.com>
2020-10-02 23:59:54 +0800

    建立首頁

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .scrapers import Pixnet
 
-# Create your views here.
+
+def index(request):
+
+    pixnet = Pixnet("千葉火鍋")
+
+    context = {
+        "articles": pixnet.get_articles()
+    }
+
+    return render(request, "articles/index.html", context)
